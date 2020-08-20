93b9f4575d55efb792140a1d33c9448789d0f952
Rafi Levy <rafi.levy@sky.com>
2020-08-20 11:14:27 +0100

    Total updated

--- public/resources/scripts/live_total.js
+++ public/resources/scripts/live_total.js
@@ -1,6 +1,6 @@
 import { CountUp } from "./countUp.min.js"
 
 window.onload = function() {
-    var countUp = new CountUp('live_total', 80_000, {"duration": 10});
+    var countUp = new CountUp('live_total', 82_148.51, {"duration": 10});
     countUp.start();
 }
